7b47824ba878004dd14c35f4143f00a48dc91e65
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-11 10:42:08 +0100

    first commit

---
+++ hello.ts
@@ -0,0 +1,9 @@
+// ex1
+let message: string = "Hello, TypeScript!";
+console.log(message);
+// ex2
+let temperature : number = 33;
+let welcomeMessage : string = "Hello, world!";
+let isLoggedIn : boolean = true;
+// ex3
+
